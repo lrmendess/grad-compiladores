1b89aeb456622e52e603e3df00fc1df69a57bb69
Lucas Ribeiro <lucasribeiromsr@gmail.com>
2019-04-17 20:33:03 -0300

    Reorganização de diretórios e makefile

---
+++ trabalho1/bateriaTestes.sh
@@ -0,0 +1,33 @@
+make
+./trab1 < in/c01.cm | diff out/c01.out -
+./trab1 < in/c02.cm | diff out/c02.out -
+./trab1 < in/c03.cm | diff out/c03.out -
+./trab1 < in/c04.cm | diff out/c04.out -
+./trab1 < in/c05.cm | diff out/c05.out -
+./trab1 < in/c06.cm | diff out/c06.out -
+./trab1 < in/c07.cm | diff out/c07.out -
+./trab1 < in/c08.cm | diff out/c08.out -
+./trab1 < in/c09.cm | diff out/c09.out -
+./trab1 < in/c10.cm | diff out/c10.out -
+./trab1 < in/c11.cm | diff out/c11.out -
+./trab1 < in/c12.cm | diff out/c12.out -
+./trab1 < in/c13.cm | diff out/c13.out -
+./trab1 < in/err1_01.cm | diff out/err1_01.out -
+./trab1 < in/err1_02.cm | diff out/err1_02.out -
+./trab1 < in/err1_03.cm | diff out/err1_03.out -
+./trab1 < in/err2_01.cm | diff out/err2_01.out -
+./trab1 < in/err2_02.cm | diff out/err2_02.out -
+./trab1 < in/err2_03.cm | diff out/err2_03.out -
+./trab1 < in/err3_04.cm | diff out/err3_04.out -
+./trab1 < in/err3_05.cm | diff out/err3_05.out -
+./trab1 < in/err3_06.cm | diff out/err3_06.out -
+./trab1 < in/err3_07.cm | diff out/err3_07.out -
+./trab1 < in/err3_08.cm | diff out/err3_08.out -
+./trab1 < in/err3_09.cm | diff out/err3_09.out -
+./trab1 < in/err3_10.cm | diff out/err3_10.out -
+./trab1 < in/err3_11.cm | diff out/err3_11.out -
+./trab1 < in/err3_12.cm | diff out/err3_12.out -
+./trab1 < in/err3_13.cm | diff out/err3_13.out -
+./trab1 < in/err3_14.cm | diff out/err3_14.out -
+./trab1 < in/err3_15.cm | diff out/err3_15.out -
+make clean
