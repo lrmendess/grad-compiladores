8a0965dd0b0afb5c1764b118f70f7df6be79605a
Lucas Ribeiro <lucasribeiromsr@gmail.com>
2019-04-29 14:47:38 -0300

    Arquivos base para começar o trabalho 2

---
+++ trabalho2/example/Lab06/Exercicio/test.sh
@@ -0,0 +1,17 @@
+./parser < in/c01.tny | diff out6/c01.out -
+./parser < in/c02.tny | diff out6/c02.out -
+./parser < in/c03.tny | diff out6/c03.out -
+./parser < in/c04.tny | diff out6/c04.out -
+./parser < in/c05.tny | diff out6/c05.out -
+
+./parser < in/err1_01.tny | diff out6/err1_01.out -
+./parser < in/err1_02.tny | diff out6/err1_02.out -
+./parser < in/err1_03.tny | diff out6/err1_03.out -
+
+./parser < in/err2_01.tny | diff out6/err2_01.out -
+./parser < in/err2_02.tny | diff out6/err2_02.out -
+./parser < in/err2_03.tny | diff out6/err2_03.out -
+
+./parser < in/err3_01.tny | diff out6/err3_01.out -
+
+
